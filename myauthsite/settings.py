b96c124982c7fa741b70fedbad201329b637468f
okdtr <okada.teru@gmail.com>
2020-03-21 20:40:52 +0800

    added dot_env

--- myauthsite/settings.py
+++ myauthsite/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 import django_heroku
+import dj_database_url
+import dotenv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-m49z+63e%iv01ugp!8y1bzixv&hjcz!b2qm4^#7g5itdm$vvh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
