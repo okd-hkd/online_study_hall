23d0cd79173b588d6425a565f9754747d90ad8d3
okdtr <okada.teru@gmail.com>
2020-03-31 15:19:30 +0800

    renewed venv

--- envOfonlinenStudyHall/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/okada/Documents/work/onlineStudyHall/django_authenticate-master/envOfonlinenStudyHall/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
